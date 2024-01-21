ae24f461fb21e54e5a2864406cb1756c6ff0b393
ddimkou <ddimkou@outlook.com>
2024-01-21 02:44:05 +0200

    initial navbar comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/contact";
 import Store from "./pages/Store";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/store">Store</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/store" element={<Store />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
