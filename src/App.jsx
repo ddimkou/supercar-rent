5a691d7dceab1bbc49ff482981fc74af20a92f93
ddimkou <ddimkou@outlook.com>
2024-01-21 02:40:52 +0200

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/contact";
+import Store from "./pages/Store";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/store">Store</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
