8b240e67e338a127a1631354a34b86b8ff7272ad
ddimkou <ddimkou@outlook.com>
2024-01-26 02:42:51 +0200

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+  const togglePanel = () => {
+    setIsPanelOpen(!isPanelOpen);
+  };
+
   return (
     <nav>
       <Link to="/">
-        <img src={logo} />
+        <img src={logo} alt="Logo" />
       </Link>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/store">Store</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/login" className="login">
-            Login
-          </Link>
-        </li>
-      </ul>
+      <button
+        className="hamburger"
+        onClick={togglePanel}
+        aria-label={isPanelOpen ? "Close menu" : "Open menu"} //accessibility
+      >
+        <FontAwesomeIcon icon={isPanelOpen ? faTimes : faBars} />
+      </button>
+      <div className={`nav-links ${isPanelOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/" onClick={() => setIsPanelOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/store" onClick={() => setIsPanelOpen(false)}>
+              Store
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => setIsPanelOpen(false)}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/login"
+              className="login"
+              onClick={() => setIsPanelOpen(false)}
+            >
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
