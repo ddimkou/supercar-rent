8b2f466f191f8b49969d23beaa8f7809562ebe4b
ddimkou <ddimkou@outlook.com>
2024-01-29 17:28:16 +0200

    scrollbar webkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./Scrollbar-webkit.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/contact";
