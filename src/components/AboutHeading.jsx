088fe8377ed93bc588ca245b6e216e3c1521a046
ddimkou <ddimkou@outlook.com>
2024-02-01 14:41:50 +0200

    about-countUp library

--- src/components/AboutHeading.jsx
+++ src/components/AboutHeading.jsx
@@ -1,34 +1,49 @@
 import storeImage from "../images/store.png";
+import CountUp from "react-countup";
 const AboutHeading = () => {
   return (
     <section className="about-heading">
       <img src={storeImage} alt="Car rental Store" />
-      <div>
-        <h2>Who we are</h2>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
-          exercitationem rerum omnis reprehenderit ducimus saepe modi molestiae
-          voluptate ea neque aut repellat velit minus porro iure, nisi alias
-          laudantium doloribus dolorum ex odio esse ipsam eveniet. Fugiat et qui
-          delectus ullam eligendi alias neque praesentium.
-        </p>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
-          eius, nulla quis nam, ex omnis voluptate placeat similique ut maxime
-          voluptas qui! Aspernatur animi facere eaque quae molestias, hic eum
-          blanditiis aperiam quisquam corrupti maiores et optio incidunt rerum
-          ipsum praesentium tempore. Vitae dolores quaerat placeat eos autem
-          molestiae, quasi, vero neque sapiente voluptatum nemo, libero natus
-          repudiandae reprehenderit quam!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi,
-          dolorem earum in esse asperiores dignissimos veniam, distinctio labore
-          cumque deleniti saepe nihil? Amet doloribus praesentium repudiandae
-          porro, voluptatem, a veritatis placeat debitis eius, ducimus
-          accusantium! Possimus molestiae nisi vero dolores adipisci, nemo
-          aliquid recusandae illo consequatur velit earum voluptatem quidem.
-        </p>
+      <div className="about-container">
+        <div className="about-text">
+          <h2>Who We Are</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa
+            exercitationem rerum omnis reprehenderit ducimus saepe modi
+            molestiae voluptate ea neque aut repellat velit minus porro iure,
+            nisi alias laudantium doloribus dolorum ex odio esse ipsam eveniet.
+            Fugiat et qui delectus ullam eligendi alias neque praesentium.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores
+            eius, nulla quis nam, ex omnis voluptate placeat similique ut maxime
+            voluptas qui! Aspernatur animi facere eaque quae molestias, hic eum
+            blanditiis aperiam quisquam corrupti maiores et optio incidunt rerum
+            ipsum praesentium tempore. Vitae dolores quaerat placeat eos autem
+            molestiae, quasi, vero neque sapiente voluptatum nemo, libero natus
+            repudiandae reprehenderit quam!
+          </p>
+        </div>
+        <div className="count-up-lbr">
+          <div className="count-countries count">
+            <div className="counting-nr">
+              <CountUp end={20} duration={2.75} />
+            </div>
+            countries
+          </div>
+          <div className="count-supercars count">
+            <div className="counting-nr">
+              <CountUp end={600} duration={2.75} />+
+            </div>
+            supercars
+          </div>
+          <div className="count-customers count">
+            <div className="counting-nr">
+              <CountUp end={100} duration={2.75} />k
+            </div>
+            customers
+          </div>
+        </div>
       </div>
     </section>
   );
