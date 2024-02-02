a0147a7c79b75123f3dfc4faf1e28ce81abd53c9
ddimkou <ddimkou@outlook.com>
2024-02-02 04:53:12 +0200

    awards

---
+++ src/components/Awards.jsx
@@ -0,0 +1,14 @@
+import award1 from "../images/awards1.png";
+import award2 from "../images/awards2.png";
+import award3 from "../images/awards3.png";
+const Awards = () => {
+  return (
+    <section className="awards">
+      <img src={award1} alt="award1" />
+      <img src={award2} alt="award3" />
+      <img src={award3} alt="award3" />
+    </section>
+  );
+};
+
+export default Awards;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import AboutHeading from "../components/AboutHeading";
 import "../About.css";
+import Awards from "../components/Awards";
 const About = () => {
   return (
     <main className="about">
       <AboutHeading />
+      <Awards />
     </main>
   );
 };
