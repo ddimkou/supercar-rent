eb3102d06dfd02a19eb528bb32327ce7da9845a5
ddimkou <ddimkou@outlook.com>
2024-02-12 12:26:58 +0200

    styling car details

--- src/components/StoreCarDetails.jsx
+++ src/components/StoreCarDetails.jsx
@@ -2,38 +2,42 @@
 const StoreCarDetails = ({ car }) => {
   return (
     <>
-      <aside className="car-image">
-        <img src={car.image} alt={`${car.manufacturer} ${car.model}`} />
-      </aside>
-      <section className="car-details-text">
+      <section className="title">
         <h2>
           <span>
             {car.manufacturer} {car.model}
           </span>
         </h2>
-        <ul className="car-specs">
-          <li>
-            <strong>Color:</strong> {car.color}
-          </li>
-          <li>
-            <strong>Year:</strong> {car.year}
-          </li>
-          <li>
-            <strong>Engine Type:</strong> {car.engine_type}
-          </li>
-          <li>
-            <strong>Horsepower:</strong> {car.horsepower} HP
-          </li>
-          <li>
-            <strong>Top Speed:</strong> {car.top_speed} mph
-          </li>
-          <li>
-            <strong>Fuel Type:</strong> {car.fuel_type}
-          </li>
-          <li>
-            <strong>Price per Day:</strong> {car.price_per_day} $
-          </li>
-        </ul>
+      </section>
+      <section className="car-details">
+        <aside className="car-image">
+          <img src={car.image} alt={`${car.manufacturer} ${car.model}`} />
+        </aside>
+        <section className="car-details-text">
+          <ul className="car-specs">
+            <li>
+              <strong>Color:</strong> {car.color}
+            </li>
+            <li>
+              <strong>Year:</strong> {car.year}
+            </li>
+            <li>
+              <strong>Engine Type:</strong> {car.engine_type}
+            </li>
+            <li>
+              <strong>Horsepower:</strong> {car.horsepower} HP
+            </li>
+            <li>
+              <strong>Top Speed:</strong> {car.top_speed} mph
+            </li>
+            <li>
+              <strong>Fuel Type:</strong> {car.fuel_type}
+            </li>
+            <li>
+              <strong>Price per Day:</strong> {car.price_per_day} $
+            </li>
+          </ul>
+        </section>
       </section>
     </>
   );
